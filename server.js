05225e977629daa1b1762b99d64ff9d82e6dc4bd
plokmij <samfan@live.com>
2016-09-17 22:42:23 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/article-one', function (req, res) {
-  console.log('Article 1 requested and will be served here');
+    res.sendFile(path.join(__dirname, 'ui', '/article-one.html'));
 });
 
 app.get('/ui/style.css', function (req, res) {
